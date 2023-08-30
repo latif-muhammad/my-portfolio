1e71646cc592d34e5762865f3ab2a5c512862c3d
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-08-30 17:15:28 +0500

    addded navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Card from './components/base_components/Card';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Card> 
         <h1>this is inside the card</h1>
       </Card>
