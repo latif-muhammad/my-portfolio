f59ae80bf520e57ca1f85638dc8df309a9955faf
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-09-13 16:33:17 +0500

    added home sections

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+const HomeScreen = () => {
+    return (
+        <div>
+            <section className="intro"></section>
+            <section className="about"></section>
+            <section className="timelineSection"></section>
+            <section className="projects"></section>
+            <section className="contactSections"></section>
+
+        </div>
+    )
+}
+export default HomeScreen;
