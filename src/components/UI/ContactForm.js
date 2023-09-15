6c8b147642e0db1988445e6cfabe2a57d9a00a34
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-09-15 17:41:20 +0500

    added header section

--- src/components/UI/ContactForm.js
+++ src/components/UI/ContactForm.js
@@ -2,17 +2,17 @@ const ContactForm = (props) => {
     return (
         <form>
             <div className="form-group mb-4">
-                <label for="name" className="mb-2 custom-sub-title">Name</label>
+                <label htmlFor="name" className="mb-2 custom-sub-title">Name</label>
                 <input type="name" className="form-control bg-transparent border-info py-2" id="contact-name"
                     placeholder="Name" />
             </div>
             <div className="form-group mb-4">
-                <label for="exampleInputEmail1" className="mb-2 custom-sub-title">Email address</label>
+                <label htmlFor="exampleInputEmail1" className="mb-2 custom-sub-title">Email address</label>
                 <input type="email" className="form-control bg-transparent border-info py-2" id="contact-email"
                     aria-describedby="emailHelp" placeholder="Email" />
             </div>
             <div className="form-group mb-4">
-                <label for="exampleFormControlTextarea1" className="mb-2 custom-sub-title">Message</label>
+                <label htmlFor="exampleFormControlTextarea1" className="mb-2 custom-sub-title">Message</label>
                 <textarea className="form-control bg-transparent border-info py-2" id="exampleFormControlTextarea1"
                     placeholder="Type Message here.." rows="4"></textarea>
             </div>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,20 @@
+
+import Navbar from '../components/core/Navbar';
+import * as data from "../utilities/data";
+import styles from "./HomeScreen.module.css";
+
 const HomeScreen = () => {
     return (
         <div>
-            <section className="intro"></section>
+            <section className={`header ${styles.header}`}>
+                <div className="overlay">
+                    <Navbar data = {data.navData}></Navbar>
+                    <div class="intro text-white vh-100 d-flex container align-items-center justify-content-center flex-column">
+                        <h1 class="mb-4 fw-bold">I am Muhammad Latif</h1>
+                        <h3 class="fw-bold ">WEB /Mobile App developer</h3>
+                    </div>
+                </div>
+            </section>
             <section className="about"></section>
             <section className="timelineSection"></section>
             <section className="projects"></section>
