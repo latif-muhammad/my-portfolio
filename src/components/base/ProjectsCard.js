1bf357ee73368f2928612768ecf5d3fa4dce5d89
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-10-04 01:26:43 +0500

    this

--- src/components/base/ProjectsCard.js
+++ src/components/base/ProjectsCard.js
@@ -4,18 +4,16 @@ const ProjectCard = (props) => {
         return window.location.href = props.link;
     }
     return (
-        <div onClick={ redirect } class={`item-box projects rounded transition bg-white overflow-hidden ${styles.projects}`}>
+        <div onClick={redirect} class={`item-box projects rounded transition bg-white overflow-hidden ${styles.projects}`}>
             <div class="body" >
-                <div style={{height: '27rem'}}>
+                <div style={{ height: '27rem' }}>
                     <img class="item-container img-fluid mx-auto" src={require(`../../assets/projects_images/${props.image}`)} alt="1" height={'25rem'} width={'100%'} />
                 </div>
-                <div class="overlay-work">
-                    <div class="work-content text-center">
-                       <i data-feather="camera" class="fea icon-sm image-icon"></i>
-                    </div>
+                <div class="text-center">
+                    <i data-feather="camera" class="fea icon-sm image-icon"></i>
                 </div>
             </div>
-            <div class="gallary-title py-4 text-center">
+            <div class="py-4 text-center">
                 <h5 className="text-dark text-uppercase"> {props.title} </h5>
             </div>
         </div>
