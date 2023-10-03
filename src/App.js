0d3a79635f32b9035707314ad614e3f14e46951f
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-10-04 01:30:21 +0500

    this

--- src/App.js
+++ src/App.js
@@ -5,57 +5,6 @@ function App() {
   return (
     
     <div className="App">
-
-        {/* <div className='row'>
-        <div class="col-lg-4 col-md-3 mt-4 picture-item" data-groups='["photography"]'>
-          <div class="item-box portfolio-box rounded shadow bg-white overflow-hidden">
-            <div class="portfolio-box-img position-relative overflow-hidden d-flex" >
-              <img class="item-container img-fluid mx-auto" src={require("./assets/projects_images/Portfolio.jpg")} alt="1" height={'85%'} width={'85%'} />
-              <div class="overlay-work">
-                <div class="work-content text-center">
-                  <a href="./assets/projects_images/Portfolio.jpg" class="lightbox text-light work-icon bg-dark d-inline-block rounded-pill "><i data-feather="camera" class="fea icon-sm image-icon"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="gallary-title py-4 text-center">
-              <h5><a href="page-portfolio-detail.html" class="title text-dark">Camera</a></h5>
-              <span>Illustrations</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-3 mt-4 picture-item" data-groups='["photography"]'>
-          <div class="item-box portfolio-box rounded shadow bg-white overflow-hidden">
-            <div class="portfolio-box-img position-relative overflow-hidden d-flex" >
-              <img class="item-container img-fluid mx-auto" src={require("./assets/projects_images/Portfolio.jpg")} alt="1" height={'350rem'} width={'350rem'} />
-              <div class="overlay-work">
-                <div class="work-content text-center">
-                  <a href="./assets/projects_images/Portfolio.jpg" class="lightbox text-light work-icon bg-dark d-inline-block rounded-pill "><i data-feather="camera" class="fea icon-sm image-icon"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="gallary-title py-4 text-center">
-              <h5 class="title text-dark">PORTFOLIO WEBSITE</h5>
-              <span>REACT, JS</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-3 mt-4 picture-item" data-groups='["photography"]'>
-          <div class="item-box portfolio-box rounded shadow bg-white overflow-hidden">
-            <div class="portfolio-box-img position-relative overflow-hidden" >
-              <img class="item-container img-fluid mx-auto" src={require("./assets/projects_images/2211.w026.n002.2759B.p1.2759.jpg")} alt="1" />
-              <div class="overlay-work">
-                <div class="work-content text-center">
-                  <a href="./assets/ab-img.png" class="lightbox text-light work-icon bg-dark d-inline-block rounded-pill "><i data-feather="camera" class="fea icon-sm image-icon"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="gallary-title py-4 text-center">
-              <h5><a href="page-portfolio-detail.html" class="title text-dark">Camera</a></h5>
-              <span>Illustrations</span>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <HomeScreen></HomeScreen>
     </div>
   );
