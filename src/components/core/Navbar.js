8f0e338764b7775b515729676e3e3a11e2ceddd8
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-10-04 01:11:48 +0500

    sd

--- src/components/core/Navbar.js
+++ src/components/core/Navbar.js
@@ -2,10 +2,6 @@ import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = (props) => {
-    const navigateTo = () => {
-        var element = document.getElementById(props.id);
-        element.scrollIntoView({ behavior: 'smooth' });
-    }
 
     const [bgColor, setBgColor] = useState('transparent');
     const [shadow, setShadow] = useState('');
@@ -30,7 +26,7 @@ const Navbar = (props) => {
         <nav className={`navbar navbar-expand-lg transition fixed-top py-2 navbar-light ${bgColor} ${shadow}`}>
             <div className={"container py-1"}>
                 <a className={"navbar-brand"} href="#Home">
-                    <img src= {require("../../assets/logo.png")} height={ 45 } alt="image not found" />
+                    <img src={require("../../assets/logo.png")} height={45} alt="not found" />
                 </a>
                 <button className={"navbar-toggler"} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className={"navbar-toggler-icon"}></span>
@@ -40,7 +36,7 @@ const Navbar = (props) => {
                         {
                             props.data.map((item) => (
                                 <li key={item.id} className={"nav-item mx-1"}>
-                                    <a className={`nav-link text-uppercase text-secondary fs-1 ${styles.link}`} href= { `#${item.title}`}>{item.title} </a>
+                                    <a className={`nav-link text-uppercase text-secondary fs-1 ${styles.link}`} href={`#${item.title}`}>{item.title} </a>
                                 </li>
                             )
                             )
