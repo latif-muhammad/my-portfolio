a27cf3f466b4fa9b2845eac9d0a33d920dbc0173
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2023-10-04 17:31:32 +0500

    fixed nav problem

--- src/components/core/Navbar.js
+++ src/components/core/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = (props) => {
                 <a className={"navbar-brand"} href="#Home">
                     <img src={require("../../assets/logo.png")} height={45} alt="not found" />
                 </a>
-                <button className={"navbar-toggler"} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className={"navbar-toggler"} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className={"navbar-toggler-icon"}></span>
                 </button>
                 <div className={"collapse navbar-collapse"} id="navbarSupportedContent">
