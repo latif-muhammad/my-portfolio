2c69866f24dbd7b9ac2fbf2884e555ea1aed9ee0
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2024-02-09 22:48:53 +0500

    fixed null exeption for smtp ids

--- src/components/UI/ContactForm.js
+++ src/components/UI/ContactForm.js
@@ -2,17 +2,17 @@ import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 
 const ContactForm = (props) => {
-    const [show, setShow] = useState(false);
-    const [successMsg, setSuccessMsg] = useState('');
+    const [showLoader, setShowLoader] = useState(false);
+    const [onSubmitMsg, setOnSubmitMsg] = useState('');
 
     const name = useRef('');
     const email = useRef('');
     const message = useRef('');
     const config = require('../../utilities/config.js');
 
-    const serviceID = config.smtp.serviceID;
-    const templateId = config.smtp.templateId;
-    const publicKey = config.smtp.publicKey;
+    const serviceID = config.smtp.serviceID ?? "";
+    const templateId = config.smtp.templateId ?? "";
+    const publicKey = config.smtp.publicKey ?? "";
 
     //USE YOUR KEYS HERE
     //CREATE ACCOUNT AT EMAILJS
@@ -25,19 +25,19 @@ const ContactForm = (props) => {
             email: email.current.value,
         }
         event.preventDefault();
-        setShow(true)
+        setShowLoader(true)
 
         emailjs.send(serviceID, templateId, data, publicKey).then(function (response) {
             if (response.status === 200) {
-                setShow(false);
-                setSuccessMsg("fa-check-circle");
+                setShowLoader(false);
+                setOnSubmitMsg("fa-check-circle");
                 email.current.value = "";
                 name.current.value = "";
                 message.current.value = "";
             }
         }, function (err) {
-            setShow(false);
-            setSuccessMsg("fa-times-circle");
+            setShowLoader(false);
+            setOnSubmitMsg("fa-times-circle");
         });
     }
 
@@ -59,7 +59,7 @@ const ContactForm = (props) => {
                     placeholder="Type Message here.." rows="4" ref={message} required></textarea>
             </div>
             {
-                show ?
+                showLoader ?
 
                     <div className="spinner-grow text-secondary" role="status">
                         <span className="sr-only text-muted"></span>
@@ -69,8 +69,8 @@ const ContactForm = (props) => {
                     <button type="submit" className="me-2 btn btn-danger px-3 py-2" >Submit</button>
             }
 
-            <span className={`p-1 ${(successMsg !== '') ? "" : "fade"}`}>
-                <i className={` ${successMsg === "fa-times-circle" ? "text-danger" : "text-success"} fs-3 fa ${successMsg}`}></i>
+            <span className={`p-1 ${(onSubmitMsg !== '') ? "" : "fade"}`}>
+                <i className={` ${onSubmitMsg === "fa-times-circle" ? "text-danger" : "text-success"} fs-3 fa ${onSubmitMsg}`}></i>
             </span>
 
         </form>
